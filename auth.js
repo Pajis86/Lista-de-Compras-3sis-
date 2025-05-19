db85c264a3594347b88dbb38f371977646a71075
Pajis86 <160757748+Pajis86@users.noreply.github.com>
2025-05-19 11:54:24 -0300

    adaaaaaa

--- auth.js
+++ auth.js
@@ -29,5 +29,12 @@ window.cadastro = async function () {
 }
 
 document.addEventListener('keydown'), function (event) {
-  if (event.key)
+  if (event.key === 'Enter') {
+    const caminho = window.location.pathname
+    if (caminho.includes('login')) {
+      login()
+    } else if (caminho.includes('cadastro')) {
+      cadastro()
+    }
+  }
 }
